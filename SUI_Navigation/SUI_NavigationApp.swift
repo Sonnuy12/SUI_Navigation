a4c7306e98521e719e4f4c7b78d7bf24b4de6be1
Son <kazanskij516@gmail.com>
2024-11-28 15:19:18 +0300

    Initial Commit

---
+++ SUI_Navigation/SUI_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUI_NavigationApp.swift
+//  SUI_Navigation
+//
+//  Created by сонный on 28.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SUI_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
